5b52c888009ae7dca63c242e9bd2f650d5cd1b2d
Daniel Furtado <danielfurtadocosta@gmail.com>
2018-04-09 18:48:56 -0300

    Add footer component and display products for layout purposes to the main page

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -1,27 +1,72 @@
 import React, { Component } from 'react';
 import logo from '../images/logo/logo_large.png';
-import '../css/structure.css';
+import Footer from '../components/footer';
 import data from '../mockups/products.json';
+import '../css/structure.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="header">
-          <img src={logo} className="travel-agency-logo" alt="logo" />
-          <span className="header-title">Travel Agency</span>
+        <header>
+          <div className="header-wrapper">
+            <img src={logo} className="travel-agency-logo" alt="logo" />
+            <span className="header-title">Travel Agency</span>
+          </div>
         </header>
-        <div className="intro">
-        {data.products[0].featured.map(item => (
-            <div key={item.location}>
-              <img src={item.imgUrl} alt={item.location}/>
-              <div>{item.location}</div>
-              <div>{item.description}</div>
+        <div className="container">
+            <h2 className="section-title">
+              <span>Featured</span>
+            </h2>
+            <div className="section-content">
+              {data.products[0].featured.map(card => (
+                <div className="card" key={card.location}>
+                  <img src={card.imgUrl} alt={card.location}/>
+                  <div className="location-title">{card.location}</div>
+                  <div>{card.description}</div>
+                  <div><i className="fas fa-user"></i>{card.currency}{card.priceOne}</div>
+                  <div><i className="fas fa-users"></i>{card.currency}{card.priceTwo}</div>
+                  <div><i className="fas fa-user-plus"></i>{card.currency}{card.priceThree}</div>
+                  <button>Book Now</button>
+                </div>
+              ))}
             </div>
-          ))}
+
+            <h2 className="section-title">
+              <span>Algarve</span>
+            </h2>
+            <div className="section-content">
+              {data.products[0].algarve.map(card => (
+                <div className="card" key={card.location}>
+                  <img src={card.imgUrl} alt={card.location}/>
+                  <div className="location-title">{card.location}</div>
+                  <div>{card.description}</div>
+                  <div><i className="fas fa-user"></i>{card.currency}{card.priceOne}</div>
+                  <div><i className="fas fa-users"></i>{card.currency}{card.priceTwo}</div>
+                  <div><i className="fas fa-user-plus"></i>{card.currency}{card.priceThree}</div>
+                  <button>Book Now</button>
+                </div>
+              ))}
+            </div>
+
+            <h2 className="section-title">
+              <span>Alentejo</span>
+            </h2>
+            <div className="section-content">
+              {data.products[0].alentejo.map(card => (
+                <div className="card" key={card.location}>
+                  <img src={card.imgUrl} alt={card.location}/>
+                  <div className="location-title">{card.location}</div>
+                  <div>{card.description}</div>
+                  <div><i className="fas fa-user"></i>{card.currency}{card.priceOne}</div>
+                  <div><i className="fas fa-users"></i>{card.currency}{card.priceTwo}</div>
+                  <div><i className="fas fa-user-plus"></i>{card.currency}{card.priceThree}</div>
+                  <button>Book Now</button>
+                </div>
+              ))}
+            </div>
         </div>
-        <footer>
-        </footer>        
+        <Footer />      
       </div>
     );
   }

fba41debef517c96d6c6783be9605b619d95397d
Daniel Furtado <danielfurtadocosta@gmail.com>
2018-04-25 07:55:14 -0300

    Adjust App.js to fetch api/products and render

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -3,39 +3,39 @@ import logo from '../images/logo/logo_large.png';
 import Cards from '../components/card-section';
 import Footer from '../components/footer';
 import '../css/index.css';
-import data from '../mockups/products.json';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      products: [
-        {
-         product: data.featured, 
-         name: "Featured", 
-         list: false 
-        },
-        {
-          product: data.algarve, 
-          name: "Algarve", 
-          list: true 
-        },
-        {
-          product: data.alentejo, 
-          name: "Alentejo", 
-          list: true 
-        }
-      ]
+      sections: [],
     };
   }
 
-  render() {
-    let section = this.state.products.map(function(section) {
-      return(
-        <Cards key={section.name} product={section.product} name={section.name} list={section.list}/>
-      );
+  componentDidMount() {
+    let self = this;
+    fetch("/api/products")
+    .then(response => {
+      if (response.status !== 200) {
+        console.log("Looks like there was a problem. Status Code: " + response.status);
+        return;
+      }
+      return response.json();
+    })
+    .then(data => {
+        let sections = data.products.map(section => {
+          return(
+            <Cards key={section.name} product={section.items} name={section.name} list={section.list}/>
+          );
+        });
+      self.setState({sections: sections});
+    })
+    .catch(function(err) {
+    console.log('Fetch Error :-S', err);
     });
+  }
 
+  render() {
     return (
       <div className="app">
         <header id="header" className="header">
@@ -45,7 +45,7 @@ class App extends Component {
           </div>
         </header>
         <main className="container">
-          {section}
+          {this.state.sections}
         </main>
         <Footer />      
       </div>

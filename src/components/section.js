71a2814a69a3b00dff18db0c5b081c91669e144b
Daniel Furtado <danielfurtadocosta@gmail.com>
2018-05-01 13:19:46 -0300

    Disable grid style button and view mode based on window width

--- src/components/section.js
+++ src/components/section.js
@@ -5,22 +5,39 @@ import Card from '../components/card';
 class Section extends Component {
   constructor(props) {
     super(props);
-    this.state = {list: this.props.list}
+    this.state = {
+      list: this.props.list,
+      grid: true
+    }
     this.handleClick = this.handleClick.bind(this);
+    this.updateDimensions = this.updateDimensions.bind(this);
   }
 
   handleClick() {
     this.setState(prevState => ({list: !prevState.list}));
   }
 
-  render() {   
+  componentDidMount() {
+    window.addEventListener("load", this.updateDimensions);
+    window.addEventListener("resize", this.updateDimensions);
+  }
+
+  updateDimensions() {
+    if(window.innerWidth < 769) {
+      this.setState({grid: false});
+    } else {
+      this.setState({grid: true});
+    }
+  }
+
+  render() { 
     return (
       <article>
         <h2 className="section-title">
-          <GridButton list={this.state.list} onClick={this.handleClick}/>
+          {this.state.grid ? <GridButton list={this.state.list} onClick={this.handleClick}/> : null}
           <span>{this.props.name}</span>
         </h2>
-        <section className={this.state.list ? "section-content flex-1-col" : "section-content flex-5-cols"}>
+        <section className={this.state.grid && this.state.list ? "section-content flex-1-col" : "section-content flex-5-cols"}>
           {this.props.product.map(card => (
             <Card 
               key={card.id} 
@@ -38,6 +55,11 @@ class Section extends Component {
       </article>
     );
   }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateDimensions);
+  }
+
 }
 
 export default Section;

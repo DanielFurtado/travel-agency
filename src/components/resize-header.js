5287c7f21e5e41dec320e4119fd59253d9fafe11
Daniel Furtado <danielfurtadocosta@gmail.com>
2018-05-01 00:33:06 -0300

    Resize header script

---
+++ src/components/resize-header.js
@@ -0,0 +1,11 @@
+export default function resizeHeader() {
+  const distanceY = window.pageYOffset || document.documentElement.scrollTop,
+        shrinkOn = 300,
+        header = document.getElementById("header");
+        
+  if (distanceY > shrinkOn) {
+    header.classList.add("shrink");
+  } else {
+    header.classList.remove("shrink");
+  }
+}
